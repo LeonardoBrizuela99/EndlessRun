c3b4c5ccc5f713c3df83e490e30c18b34578d383
leo <leo99brizuela@gmail.com>
2024-04-16 16:49:39 -0300

    Include .bat

---
+++ commands.bat
@@ -0,0 +1,7 @@
+xcopy "%CD%\sfml-audio-d-2.dll" "%CD%\bin\Debug\" /y /i
+xcopy "%CD%\sfml-graphics-d-2.dll" "%CD%\bin\Debug\" /y /i
+xcopy "%CD%\sfml-network-d-2.dll" "%CD%\bin\Debug\" /y /i
+xcopy "%CD%\sfml-system-d-2.dll" "%CD%\bin\Debug\" /y /i
+xcopy "%CD%\sfml-window-d-2.dll" "%CD%\bin\Debug\" /y /i
+
+
