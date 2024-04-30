6d09d0d58fb86297f010c9027627d00fe5808306
Leo <leo99brizuela@gmail.com>
2024-04-30 16:49:35 -0300

    fix x64

--- commands.bat
+++ commands.bat
@@ -1,8 +1,18 @@
-set origenDebug="C:\Users\Aula 1\Desktop\EndlessRun\SFML\dlls\x86\debug"
-set origenRelease="C:\Users\Aula 1\Desktop\EndlessRun\SFML\dlls\x86\release"
+set origenDebug86="C:\Users\Aula 1\Desktop\EndlessRun\SFML\dlls\x86\debug"
+set origenRelease86="C:\Users\Aula 1\Desktop\EndlessRun\SFML\dlls\x86\release"
 
-set debug="C:\Users\Aula 1\Desktop\EndlessRun\Debug"
-set release="C:\Users\Aula 1\Desktop\EndlessRun\Release"
+set debug86="C:\Users\Aula 1\Desktop\EndlessRun\Debug"
+set release86="C:\Users\Aula 1\Desktop\EndlessRun\Release"
 
-copy %origenDebug%\*.dll %debug%
-copy %origenRelease%\*.dll %release%
+copy %origenDebug86%\*.dll %debug86%
+copy %origenRelease86%\*.dll %release86%
+
+
+set origenDebug64="C:\Users\Aula 1\Desktop\EndlessRun\SFML\dlls\x64\debug"
+set origenRelease64="C:\Users\Aula 1\Desktop\EndlessRun\SFML\dlls\x64\release"
+
+set debug64="C:\Users\Aula 1\Desktop\EndlessRun\x64\Debug"
+set release64="C:\Users\Aula 1\Desktop\EndlessRun\x64\Release"
+
+copy %origenDebug64%\*.dll %debug64%
+copy %origenRelease64%\*.dll %release64%
